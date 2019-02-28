f21e986604718a0bea058068baafbf2789aacd49
Kirill Maksimov <kirill@orbs.com>
2019-02-28 15:13:37 +0200

    Initial implementation

---
+++ README.md
@@ -0,0 +1,8 @@
+# Status page
+
+## Configuration
+
+Environment variables:
+
+    export IPS='{"node-name":"1.2.3.4"}'
+    export VCHAINS=1000,2000,3000
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const fs = require('fs');
+const _ = require('lodash');
+const { getStatus, getEndpoint } = require('nebula/lib/metrics');
+
+const vchains = process.env.VCHAINS.split(",");
+const ips = JSON.parse(process.env.IPS);
+
+const state = {
+    
+};
+
+function query() {
+    _.map(ips, async (ip, node) => {
+        const aggregatedRequest = _.reduce(vchains, (result, vchain) => {
+            result[vchain] = getEndpoint(ip, vchain);
+            return result;
+        }, {});
+
+        
+        state[node] = await getStatus(aggregatedRequest, 5000, 60000);
+        console.log(state[node]);
+    })
+}
+
+function placeholder() {
+    _.map(ips, async (ip, node) => {
+        state[node] = _.reduce(vchains, (result, vchain) => {
+            result[vchain] = {};
+            return result;
+        }, {});
+    });
+}
+
+placeholder();
+
+setInterval(query, 30000);
+query();
+
+app.get('/', (req, res) => {
+    const template = _.template(fs.readFileSync('./templates/index.html').toString());
+    res.send(template({
+        state,
+        vchains
+    }));
+});
+
+app.listen(port, () => console.log(`Status page listening on port ${port}!`))
