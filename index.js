54b26cc89a513de20355f613bed8a10c323666c9
Kirill Maksimov <kirill@orbs.com>
2019-10-22 15:24:14 +0300

    Update visuals, get configuration from Kartoha

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const port = process.env.PORT || 3000;
-const fs = require('fs');
-const _ = require('lodash');
-const { getStatus, getEndpoint } = require('nebula/lib/metrics');
+const fs = require("fs");
+const _ = require("lodash");
+const { getStatus, getEndpoint } = require("@orbs-network/orbs-nebula/lib/metrics");
 
 const vchains = process.env.VCHAINS.split(",");
-const ips = JSON.parse(process.env.IPS);
+const ips = JSON.parse(process.env.IPS || `{"nodes":[]}`).nodes;
 
 const state = {
     
 };
 
 function query() {
-    _.map(ips, async (ip, node) => {
+    _.map(ips, async ({ name, host }) => {
         const aggregatedRequest = _.reduce(vchains, (result, vchain) => {
-            result[vchain] = getEndpoint(ip, vchain);
+            result[vchain] = getEndpoint(host, vchain);
             return result;
         }, {});
 
-        
-        state[node] = await getStatus(aggregatedRequest, 5000, 60000);
-        console.log(state[node]);
+
+        state[name] = await getStatus(aggregatedRequest, 5000, 60000);
+        console.log(state[name]);
     })
 }
 
 function placeholder() {
-    _.map(ips, async (ip, node) => {
-        state[node] = _.reduce(vchains, (result, vchain) => {
+    _.map(ips, async ({ name, host }) => {
+        state[name] = _.reduce(vchains, (result, vchain) => {
             result[vchain] = {};
             return result;
         }, {});
@@ -36,11 +36,13 @@ function placeholder() {
 
 placeholder();
 
-setInterval(query, 30000);
+setInterval(query, 10000);
 query();
 
-app.get('/', (req, res) => {
-    const template = _.template(fs.readFileSync('./templates/index.html').toString());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    const template = _.template(fs.readFileSync("./templates/index.html").toString());
     res.send(template({
         state,
         vchains,
