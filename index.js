4693d4d4709ba91e513f652830a5193d1c1973fd
Kirill Maksimov <kirill@orbs.com>
2019-12-01 15:36:17 +0200

    Add db

--- index.js
+++ index.js
@@ -7,42 +7,40 @@ const { getStatus, getEndpoint } = require("@orbs-network/orbs-nebula/lib/metric
 
 const vchains = process.env.VCHAINS.split(",");
 const ips = JSON.parse(process.env.IPS || `{"nodes":[]}`).nodes;
+const recordsRetention = Number(process.env.RETENTION || 60);
 
-const state = {
-    
-};
+const db = require("./db");
 
 function query() {
-    _.map(ips, async ({ name, host }) => {
-        const aggregatedRequest = _.reduce(vchains, (result, vchain) => {
-            result[vchain] = getEndpoint(host, vchain);
-            return result;
-        }, {});
+    _.map(ips, async ({ name, host }) => {        
+        const batch = await db.getLastBatch();
 
-
-        state[name] = await getStatus(aggregatedRequest, 5000, 60000);
-        console.log(state[name]);
+        _.map(vchains, async (vchain) => {
+            try {
+                const status = await getStatus({data: getEndpoint(host, vchain)}, 5000, 60000);
+                await db.addStatus(batch + 1, name, Number(vchain), status.data);
+            } catch (e) {
+                console.log(e);
+            }
+        })
     })
 }
 
-function placeholder() {
-    _.map(ips, async ({ name, host }) => {
-        state[name] = _.reduce(vchains, (result, vchain) => {
-            result[vchain] = {};
-            return result;
-        }, {});
-    });
+function cleanup() {
+    db.removeOldRecords(recordsRetention);
 }
 
-placeholder();
+setInterval(query, 60000);
+db.migrate().then(query);
 
-setInterval(query, 10000);
-query();
+setInterval(cleanup, 60000);
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
     const template = _.template(fs.readFileSync("./templates/index.html").toString());
+    const state = await db.getStatus();
+
     res.send(template({
         state,
         vchains,
