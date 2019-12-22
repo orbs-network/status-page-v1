a5af110edd0e0614911719d6ed6925c11859f7a6
Kirill Maksimov <kirill@orbs.com>
2019-12-22 11:25:52 +0200

    Continue with scaffold

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -1,8 +1,15 @@
 <script>
-import { size } from "lodash";
+import { size, isEmpty } from "lodash";
+import Info from "./Info.svelte";
 
 export let vchains;
+export let validators;
 
+const getDescription = (vcid) => "";
+
+const getData = (validator, vcid) => {
+    return { blockHeight: 999999, version: "v1.3.6", status: "green" }
+};
 </script>
 
 <div class="centeredDiv" style="width: {(size(vchains) + 1) * 150}px;">
@@ -12,4 +19,24 @@ export let vchains;
         Network Status
         </h1>
     </div>
+
+    <table>
+        <tr class="thead">
+            <td><!-- empty space --></td>
+            {#each vchains as vcid}                
+                <td><span class="vcid">{vcid}</span>
+                <br/><span class="description">{getDescription(vcid)}</span>
+                </td>
+            {/each}
+        </tr>
+    
+        {#each validators as validator}
+        <tr>
+            <td class="node-name">{validator}</td>
+            {#each vchains as vcid}
+                <Info data={getData(validator, vcid)} vcid={vcid} host="test" />
+            {/each}
+        </tr>
+        {/each}
+    </table>
 </div>
--- src/client/main.js
+++ src/client/main.js
@@ -4,6 +4,7 @@ const app = new App({
     target: document.body,
     props: {
       vchains: [1000, 1003, 2000],
+      validators: ["node1", "node2", "node3"],
     }
   });
   
